9d70caf880236e9d4d9f7081ec996f2c479d258e
jtmoreno <juancdagarra@gmail.com>
2023-08-21 11:40:18 -0500

    teminado todas la puebas modulo3

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -2,52 +2,68 @@
 /*5️⃣ EJERCICIO 05 5️⃣*/
 
 function esPositivo(num) {
-   // La función recibe un entero. Devuelve como resultado un string que indica si el número
-   // es positivo o negativo.
-   // Si el número es positivo ---> "Es positivo".
-   // Si el número es negativo ---> "Es negativo".
-   // Si el número es 0, devuelve false.
-   // Tu código:
+  // La función recibe un entero. Devuelve como resultado un string que indica si el número
+  // es positivo o negativo.
+  // Si el número es positivo ---> "Es positivo".
+  // Si el número es negativo ---> "Es negativo".
+  // Si el número es 0, devuelve false.
+  // Tu código:
+  if (num > 0) {
+    return "Es positivo";
+  }
+  if (num < 0) {
+    return "Es negativo";
+  } else {
+    return false;
+  }
+  return false;
 }
 
 function agregarSimboloExclamacion(str) {
-   // Agrega un símbolo de exclamación al final del string "str" y retórnalo
-   // Ejemplo: "hello world" ---> "hello world!"
-   // Tu código:
+  // Agrega un símbolo de exclamación al final del string "str" y retórnalo
+  // Ejemplo: "hello world" ---> "hello world!"
+  // Tu código:
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
-   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
-   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
-   // Tu código:
+  // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
+  // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
+  // Tu código:
+  return nombre + " " + apellido;
 }
 
 function obtenerSaludo(nombre) {
-   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
-   // Ejemplo: "Martin" ---> "Hola Martin!"
-   // Tu código:
+  // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
+  // Ejemplo: "Martin" ---> "Hola Martin!"
+  // Tu código:
+  return "Hola " + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
-   // Retornar el área de un rectángulo teniendo su altura y ancho.
-   // Tu código:
+  // Retornar el área de un rectángulo teniendo su altura y ancho.
+  // Tu código:
+  return alto * ancho;
 }
 
 function retornarPerimetro(lado) {
-   // La función recibe como argumento la medida de un lado de un cuadrado.
-   // Debes retornar su perímetro.
-   // Tu código:
+  // La función recibe como argumento la medida de un lado de un cuadrado.
+  // Debes retornar su perímetro.
+  // Tu código:
+  return lado * 4;
 }
 
 function areaDelTriangulo(base, altura) {
-   // Calcula el área de un triángulo y retorna el resultado.
-   // Tu código:
+  // Calcula el área de un triángulo y retorna el resultado.
+  // Tu código:
+  return (base * altura) / 2;
 }
 
 function deEuroAdolar(euro) {
-   // Supongamos que 1 euro equivale a 1.20 dólares.
-   // Debes calcular el valor recibido como argumento pasándolo a dolares.
-   // Tu código:
+  // Supongamos que 1 euro equivale a 1.20 dólares.
+  // Debes calcular el valor recibido como argumento pasándolo a dolares.
+  // Tu código:
+  return euro * 1.2;
 }
 
 function esVocal(letra) {
@@ -55,17 +71,43 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   let longitud = letra.length;
+   let ret="Es vocal"
+   if (longitud == 1) {
+
+       console.log("if")
+     switch (letra) {
+       case 'a':
+         console.log('a adentro switch')
+         return ret;
+ 
+       case 'e':
+         return "Es vocal";
+       case 'i':
+         return "Es vocal";
+       case 'o':
+         return "Es vocal";
+       case 'u':
+         return "Es vocal";
+         
+     }
+     return "Dato incorrecto";
+   } else {
+       console.log("Else")
+       return "Dato incorrecto";
+
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   esPositivo,
-   agregarSimboloExclamacion,
-   combinarNombres,
-   obtenerSaludo,
-   obtenerAreaRectangulo,
-   retornarPerimetro,
-   areaDelTriangulo,
-   deEuroAdolar,
-   esVocal,
+  esPositivo,
+  agregarSimboloExclamacion,
+  combinarNombres,
+  obtenerSaludo,
+  obtenerAreaRectangulo,
+  retornarPerimetro,
+  areaDelTriangulo,
+  deEuroAdolar,
+  esVocal,
 };
